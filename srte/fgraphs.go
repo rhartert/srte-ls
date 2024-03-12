647c644e5ea7429801adf309639aae7791df85d9
Renaud Hartert <91352809+rhartert@users.noreply.github.com>
2024-03-12 15:04:51 +0900

    Add FGraphs structure + tests
    This commit adds the forwarding graph data structure as well as the code to compute it from a Digraph.

---
+++ srte/fgraphs.go
@@ -0,0 +1,185 @@
+package srte
+
+import (
+	"fmt"
+	"math"
+	"sort"
+
+	"github.com/rhartert/yagh"
+)
+
+// EdgeRatio represent an edge in a forwarding graph and the ratio of load sent
+// on that edge. For example, EdgeRatio{5, 0.5} means that 50% of the load sent
+// on the forwarding graph traverses edge 5.
+type EdgeRatio struct {
+	Edge  int
+	Ratio float64
+}
+
+type FGraphs struct {
+	edgesRatios [][][]EdgeRatio
+}
+
+// EdgeRatios returns the list of EdgeRatio pairs on the forwarding graph from
+// node s to node t.
+func (fgs *FGraphs) EdgeRatios(s int, t int) []EdgeRatio {
+	return fgs.edgesRatios[s][t]
+}
+
+func NewFGraphs(g *Digraph) (*FGraphs, error) {
+	nNodes := len(g.Nexts)
+
+	fgs := &FGraphs{
+		edgesRatios: make([][][]EdgeRatio, nNodes),
+	}
+
+	for u := 0; u < nNodes; u++ {
+		fgs.edgesRatios[u] = make([][]EdgeRatio, nNodes)
+
+		prevs, err := shortestDAG(g, u)
+		if err != nil {
+			return nil, err
+		}
+
+		for v := 0; v < nNodes; v++ {
+			if u == v {
+				continue
+			}
+			edgeRatiosMap := forwardingGraph(g, prevs, u, v)
+			fgs.edgesRatios[u][v] = make([]EdgeRatio, 0, len(edgeRatiosMap))
+			for e, r := range edgeRatiosMap {
+				fgs.edgesRatios[u][v] = append(fgs.edgesRatios[u][v], EdgeRatio{
+					Edge:  e,
+					Ratio: r,
+				})
+			}
+			sort.Slice(fgs.edgesRatios[u][v], func(i, j int) bool {
+				return fgs.edgesRatios[u][v][i].Edge < fgs.edgesRatios[u][v][j].Edge
+			})
+		}
+	}
+
+	return fgs, nil
+}
+
+// foardingGraph computes the fraction of load sent on each edge when sending
+// traffic from node s to node t.
+//
+// The returned load must respect the following invariants where loadIn[n] is
+// the total amount of load on edges reaching node n and loadOut[n] is the total
+// amount of load on edges leaving n:
+//   - loadIn[s] = 1 and loadOut[s] = 0,
+//   - loadIn[t] = 0 and loadOut[t] = 1,
+//   - loadIn[n] = loadOut[n] for all node n != s, t.
+//
+// The algorithm operates in two phase. The first phase traverses prevs
+// from t to s to compute the DAG of all the shortest paths from s to t.
+// The second phase traverses that DAG to compute the fraction of load sent
+// over each edge from s to t.
+//
+// Compute the fraction of traffic sent on each edge. For any edge (u, v),
+// the total fraction of traffic received at node u must be computed before
+// computing the fraction sent on the edge. This is done by processing the
+// nodes in their topological order.
+func forwardingGraph(g *Digraph, prevs [][]int, s int, t int) map[int]float64 {
+	queue := []int{} // used by both steps below
+	nNodes := len(g.Nexts)
+
+	// Step 1: extract DAG
+	// -------------------
+	nexts := make([][]int, nNodes)
+	degrees := make([]int, nNodes)
+
+	queue = append(queue, t)
+	for i := 0; i < len(queue); i++ {
+		v := queue[i]
+		degrees[v] = len(prevs[v])
+		for _, e := range prevs[v] {
+			u := g.Edges[e].From
+			queue = append(queue, u)
+			nexts[u] = append(nexts[u], e)
+		}
+	}
+
+	// Step 2: Compute load ratios
+	// ---------------------------
+	nodeLoad := make([]float64, nNodes)
+	edgeLoad := make(map[int]float64) // result
+
+	queue = queue[:0] // reset
+	queue = append(queue, s)
+	nodeLoad[s] = 1.0
+	for i := 0; i < len(queue); i++ {
+		u := queue[i]
+		for _, e := range nexts[u] {
+			v := g.Edges[e].To
+
+			l := nodeLoad[u] / float64(len(nexts[u]))
+			edgeLoad[e] = l
+			nodeLoad[v] += l
+
+			degrees[v] -= 1
+			if degrees[v] == 0 {
+				queue = append(queue, v)
+			}
+		}
+	}
+
+	return edgeLoad
+}
+
+// shortestDAG computes and returns a DAG that encapsulates the shortest paths
+// from a specified source node src to all other nodes within the digraph g.
+//
+// This function returns a slice that maps each node v in the graph o a list of
+// incoming edges (u, v), where each edge represents a part of the shortest path
+// from src to v. If a node v is unreachable from src, its corresponding list
+// will be empty.
+func shortestDAG(g *Digraph, src int) ([][]int, error) {
+	if g == nil {
+		return nil, fmt.Errorf("digraph is nil")
+	}
+
+	nNodes := len(g.Nexts)
+	if src < 0 || nNodes <= src {
+		return nil, fmt.Errorf("node %d is not in the graph", src)
+	}
+
+	prevs := make([][]int, nNodes)
+	costs := make([]int, nNodes)
+	for i := range costs {
+		costs[i] = math.MaxInt
+	}
+
+	h := yagh.New[int](nNodes)
+	h.Put(src, 0)
+	costs[src] = 0
+
+	for h.Size() > 0 {
+		entry := h.Pop()
+		u, c := entry.Elem, entry.Cost
+
+		for _, e := range g.Nexts[u] {
+			newCost := c + g.Edges[e].Cost
+			v := g.Edges[e].To
+
+			// Path src -> u -> v is worse than the best known path.
+			if costs[v] < newCost {
+				continue
+			}
+
+			// Path src -> u -> v is one of the best paths to v so far.
+			if costs[v] == newCost {
+				prevs[v] = append(prevs[v], e)
+				continue
+			}
+
+			// Path src -> u -> v is the better than the best path to v so far.
+			costs[v] = newCost
+			prevs[v] = []int{e}
+			h.Put(v, newCost)
+		}
+	}
+
+	return prevs, nil
+}
---
+++ srte/fgraphs_test.go
@@ -0,0 +1,271 @@
+package srte
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFGraphs_EdgeRatios(t *testing.T) {
+	want := []EdgeRatio{{1, 0.1}, {2, 0.2}, {3, 0.3}}
+	fgs := FGraphs{
+		edgesRatios: [][][]EdgeRatio{{{}, want}},
+	}
+
+	got := fgs.EdgeRatios(0, 1)
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("EdgeRatios(): mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		graph   *Digraph
+		want    *FGraphs
+		wantErr bool
+	}{
+		{
+			desc:  "empty graph",
+			graph: NewDigraph(nil, 0),
+			want: &FGraphs{
+				[][][]EdgeRatio{},
+			},
+		},
+		{
+			desc:  "single node",
+			graph: NewDigraph(nil, 1),
+			want: &FGraphs{
+				[][][]EdgeRatio{{nil}},
+			},
+		},
+		{
+			// 0-->1
+			desc:  "one edge",
+			graph: NewDigraph([]Edge{{0, 1, 0}}, 2),
+			want: &FGraphs{
+				[][][]EdgeRatio{
+					{
+						nil,                   // 0 -> 0
+						{{Edge: 0, Ratio: 1}}, // 0 -> 1
+					},
+					{
+						{},  // 1 -> 0
+						nil, // 1 -> 1
+					},
+				},
+			},
+		},
+		{
+			// 0-->1   2-->3
+			desc:  "not connected",
+			graph: NewDigraph([]Edge{{0, 1, 1}, {2, 3, 1}}, 4),
+			want: &FGraphs{
+				[][][]EdgeRatio{
+					{
+						nil,                   // 0 -> 0
+						{{Edge: 0, Ratio: 1}}, // 0 -> 1
+						{},                    // 0 -> 2
+						{},                    // 0 -> 3
+					},
+					{
+						{},  // 1 -> 0
+						nil, // 1 -> 1
+						{},  // 1 -> 2
+						{},  // 1 -> 3
+					},
+					{
+						{},                    // 2 -> 0
+						{},                    // 2 -> 1
+						nil,                   // 2 -> 2
+						{{Edge: 1, Ratio: 1}}, // 2 -> 3
+					},
+					{
+						{},  // 3 -> 0
+						{},  // 3 -> 1
+						{},  // 3 -> 2
+						nil, // 3 -> 3
+					},
+				},
+			},
+		},
+		{
+			// 0<->1<->2
+			// ^       ^
+			// |       |
+			// +-->3<--+
+			desc: "strongly connected",
+			graph: NewDigraph([]Edge{
+				{0, 1, 1}, // edge: 0
+				{1, 0, 1}, // edge: 1
+				{1, 2, 1}, // edge: 2
+				{2, 1, 1}, // edge: 3
+				{0, 3, 1}, // edge: 4
+				{3, 0, 1}, // edge: 5
+				{2, 3, 1}, // edge: 6
+				{3, 2, 1}, // edge: 7
+			}, 4),
+			want: &FGraphs{
+				[][][]EdgeRatio{
+					{
+						nil,                                      // 0 -> 0
+						{{0, 1}},                                 // 0 -> 1
+						{{0, 0.5}, {2, 0.5}, {4, 0.5}, {7, 0.5}}, // 0 -> 2
+						{{4, 1}},                                 // 0 -> 3
+					},
+					{
+						{{1, 1}},                                 // 1 -> 0
+						nil,                                      // 1 -> 1
+						{{2, 1}},                                 // 1 -> 2
+						{{1, 0.5}, {2, 0.5}, {4, 0.5}, {6, 0.5}}, // 1 -> 3
+					},
+					{
+						{{1, 0.5}, {3, 0.5}, {5, 0.5}, {6, 0.5}}, // 2 -> 0
+						{{3, 1}},                                 // 2 -> 1
+						nil,                                      // 2 -> 2
+						{{6, 1}},                                 // 2 -> 3
+					},
+					{
+						{{5, 1}},                                 // 3 -> 0
+						{{0, 0.5}, {3, 0.5}, {5, 0.5}, {7, 0.5}}, // 3 -> 1
+						{{7, 1}},                                 // 3 -> 2
+						nil,                                      // 3 -> 3
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, gotErr := NewFGraphs(tc.graph)
+
+			if tc.wantErr && gotErr == nil {
+				t.Errorf("New(): want error, got nil")
+			}
+			if !tc.wantErr && gotErr != nil {
+				t.Errorf("New(): want no error, got %s", gotErr)
+			}
+			if diff := cmp.Diff(tc.want.edgesRatios, got.edgesRatios); diff != "" {
+				t.Errorf("New(): mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestNew_shortestPathDAG(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		graph   *Digraph
+		src     int
+		want    [][]int
+		wantErr bool
+	}{
+		{
+			desc:    "nil graph",
+			wantErr: true,
+		},
+		{
+			desc:    "empty graph",
+			graph:   NewDigraph(nil, 0),
+			wantErr: true,
+		},
+		{
+			desc:  "single node (no edge)",
+			graph: NewDigraph(nil, 1),
+			want:  [][]int{nil},
+		},
+		{
+			// 0-->1
+			desc:  "one edge",
+			graph: NewDigraph([]Edge{{0, 1, 0}}, 2),
+			want:  [][]int{nil, {0}},
+		},
+		{
+			// 0-->1   2-->3
+			desc:  "not connected",
+			graph: NewDigraph([]Edge{{0, 1, 1}, {2, 3, 1}}, 4),
+			want:  [][]int{nil, {0}, nil, nil},
+		},
+		{
+			// 0-->1-->2
+			//  \      ^
+			//   \     |
+			//    +----+
+			desc: "one shortest path (A)",
+			graph: NewDigraph([]Edge{
+				{0, 1, 1},
+				{1, 2, 1},
+				{0, 2, 3},
+			}, 3),
+			want: [][]int{nil, {0}, {1}},
+		},
+		{
+			// 0-->1-->2
+			//  \      ^
+			//   \     |
+			//    +----+
+			desc: "one shortest path (B)",
+			graph: NewDigraph([]Edge{
+				{0, 1, 1},
+				{1, 2, 1},
+				{0, 2, 1},
+			}, 3),
+			want: [][]int{nil, {0}, {2}},
+		},
+		{
+			// 0-->1-->2
+			//  \      ^
+			//   \     |
+			//    +----+
+			desc: "two shortest paths",
+			graph: NewDigraph([]Edge{
+				{0, 1, 1},
+				{1, 2, 1},
+				{0, 2, 2},
+			}, 3),
+			want: [][]int{nil, {0}, {2, 1}},
+		},
+		{
+			// 0-->1-->2-->3
+			// |   ^       ^
+			// |   |       |
+			// +-->4------>5
+			desc: "three shortest paths",
+			graph: NewDigraph([]Edge{
+				{0, 1, 2}, // edge: 0
+				{1, 2, 2}, // edge: 1
+				{2, 3, 1}, // edge: 2
+				{0, 4, 1}, // edge: 3
+				{4, 1, 1}, // edge: 4
+				{4, 5, 3}, // edge: 5
+				{5, 3, 1}, // edge: 6
+			}, 6),
+			want: [][]int{
+				nil,
+				{0, 4},
+				{1},
+				{6, 2},
+				{3},
+				{5},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, gotErr := shortestDAG(tc.graph, tc.src)
+
+			if tc.wantErr && gotErr == nil {
+				t.Errorf("shortestDAG(): want error, got nil")
+			}
+			if !tc.wantErr && gotErr != nil {
+				t.Errorf("shortestDAG(): want no error, got %s", gotErr)
+			}
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("shortestDAG(): mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
