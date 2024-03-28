45f9b7708dabf0d0e85783cfded595f4d8eb9526
Renaud Hartert <91352809+rhartert@users.noreply.github.com>
2024-03-28 12:11:14 +0900

    Refactor and improve performance of state.go
    This commit slightly refactor the `state.State` (which is now renamed `NetworkState`) to improve its performance.
    
    The main change is to rely on a simpler data structure than the sparse set. That new data structure allows to maintain the set of changed edges without having to perform swaps.

--- srte/state.go
+++ srte/state.go
@@ -1,72 +1,87 @@
 package srte
 
-import "github.com/rhartert/sparsesets"
-
-// State is a reversible structure which represents the state of the network in
-// terms of traffic. This structure keep track of changes applied to its edges
-// and can efficiently undo them.
-type State struct {
-	loads      []int64
-	savedLoads []int64
-	savedEdges *sparsesets.Set
+type LoadChange struct {
+	Edge      int
+	SavedLoad int64
 }
 
-// New initializes and returns a new State.
-func NewState(nEdges int) *State {
-	return &State{
-		loads:      make([]int64, nEdges),
-		savedLoads: make([]int64, nEdges),
-		savedEdges: sparsesets.New(nEdges),
+// NetworkState is a reversible structure which represents the state of the
+// network in terms of traffic. This structure keeps track of changes applied
+// to its edges and can efficiently undo them.
+type NetworkState struct {
+	loads []int64
+
+	// Stack of changes applied to the current state.
+	changes  []LoadChange
+	nChanges int
+
+	// The savedAt slice effectively acts as a slice of booleans to check
+	// whether the load of an edge was changed in the current state or not.
+	// Precisely, an edge e has been changed if savedAt[e] == timestamp. The
+	// use of logical timestamp (rather than booleans) provides an efficient
+	// way to mark all edges as unchanged in O(1) by incrementing the timestamp.
+	savedAt   []int
+	timestamp int
+}
+
+// NewNetworkState initializes and returns a new NetworkState.
+func NewNetworkState(nEdges int) *NetworkState {
+	return &NetworkState{
+		loads:     make([]int64, nEdges),
+		changes:   make([]LoadChange, nEdges),
+		nChanges:  0,
+		savedAt:   make([]int, nEdges),
+		timestamp: 1, // must be different than the values in savedAt
 	}
 }
 
 // Load returns the current load on the edge.
-func (s State) Load(edge int) int64 {
+func (s *NetworkState) Load(edge int) int64 {
 	return s.loads[edge]
 }
 
 // AddLoad adds the load from the edge. The change is registered so that it
 // can be undone if needed.
-func (s State) AddLoad(edge int, load int64) {
-	if !s.savedEdges.Contains(edge) {
-		s.savedEdges.Insert(edge)
-		s.savedLoads[edge] = s.loads[edge]
+func (s *NetworkState) AddLoad(edge int, load int64) {
+	if s.savedAt[edge] != s.timestamp {
+		s.changes[s.nChanges] = LoadChange{edge, s.loads[edge]}
+		s.nChanges += 1
+		s.savedAt[edge] = s.timestamp
 	}
 	s.loads[edge] += load
 }
 
 // RemoveLoad removes the load from the edge. The change is registered so that
 // it can be undone if needed.
-func (s State) RemoveLoad(edge int, load int64) {
-	if !s.savedEdges.Contains(edge) {
-		s.savedEdges.Insert(edge)
-		s.savedLoads[edge] = s.loads[edge]
-	}
-	s.loads[edge] -= load
+func (s *NetworkState) RemoveLoad(edge int, load int64) {
+	s.AddLoad(edge, -load)
 }
 
 // PersistChanges persists all the changes as the "new" state. New changes can
 // be accumulated (and undone) from this point.
-func (s *State) PersistChanges() {
-	s.savedEdges.Clear()
+func (s *NetworkState) PersistChanges() {
+	s.nChanges = 0
+	s.timestamp += 1
 }
 
 // UndoChanges undoes all the changes since the last time PersistChanges was
 // called. This operation is done in O(C) where C is the number of edges that
 // have been changed.
-func (s *State) UndoChanges() {
-	for _, e := range s.savedEdges.Content() {
-		s.loads[e] = s.savedLoads[e]
+func (s *NetworkState) UndoChanges() {
+	for s.nChanges > 0 {
+		s.nChanges -= 1
+		lc := s.changes[s.nChanges]
+		s.loads[lc.Edge] = lc.SavedLoad
 	}
-	s.savedEdges.Clear()
+	s.timestamp += 1
 }
 
-// ChangedEdges returns the edges that have been changed since the last time
+// Changes returns the edges that have been changed since the last time
 // changes were persisted.
 //
 // Impirtant: the slice is a view on one of the state's internal structure and
 // should only be used in read-only operations. Modifying the slice will most
 // likely results in incorrect behavior.
-func (s *State) ChangedEdges() []int {
-	return s.savedEdges.Content()
+func (s *NetworkState) Changes() []LoadChange {
+	return s.changes[:s.nChanges]
 }
--- srte/state_test.go
+++ srte/state_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestState_Load(t *testing.T) {
-	state := NewState(3)
+	state := NewNetworkState(3)
 	want := int64(100)
 	state.loads[1] = want
 
@@ -19,126 +19,132 @@ func TestState_Load(t *testing.T) {
 }
 
 func TestState_AddLoad_oneAdd(t *testing.T) {
-	state := NewState(3)
+	wantChanges := []LoadChange{{0, 0}}
+	wantLoads := []int64{10, 0, 0}
+	state := NewNetworkState(3)
 
 	state.AddLoad(0, 10)
+	gotChanges := state.Changes()
 
-	if got := state.loads[0]; got != 10 {
-		t.Errorf("Load of edge 0 should be 10, got %d", got)
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if !state.savedEdges.Contains(0) {
-		t.Errorf("Edge 0 should be saved")
-	}
-	if got := state.savedLoads[0]; got != 0 {
-		t.Errorf("Saved load of edge 0 should be 0, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestState_AddLoad_twoAdds(t *testing.T) {
-	state := NewState(3)
+	wantChanges := []LoadChange{{1, 10}}
+	wantLoads := []int64{0, 30, 0}
+	state := NewNetworkState(3)
 	state.loads[1] = 10
 
 	state.AddLoad(1, 10)
 	state.AddLoad(1, 10)
+	gotChanges := state.Changes()
 
-	if got := state.loads[1]; got != 30 {
-		t.Errorf("Load of edge 1 should be 30, got %d", got)
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if !state.savedEdges.Contains(1) {
-		t.Errorf("Edge 1 should be saved")
-	}
-	if got := state.savedLoads[1]; got != 10 {
-		t.Errorf("Saved load of edge 1 should be 10, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestState_RemoveLoad_oneRemove(t *testing.T) {
-	state := NewState(3)
+	wantChanges := []LoadChange{{0, 10}}
+	wantLoads := []int64{0, 0, 0}
+	state := NewNetworkState(3)
 	state.loads[0] = 10
 
 	state.RemoveLoad(0, 10)
+	gotChanges := state.Changes()
 
-	if got := state.loads[0]; got != 0 {
-		t.Errorf("Load of edge 0 should be 0, got %d", got)
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if !state.savedEdges.Contains(0) {
-		t.Errorf("Edge 0 should be saved")
-	}
-	if got := state.savedLoads[0]; got != 10 {
-		t.Errorf("Saved load of edge 0 should be 10, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestState_RemoveLoad_twoRemoves(t *testing.T) {
-	state := NewState(3)
+	wantChanges := []LoadChange{{1, 30}}
+	wantLoads := []int64{0, 10, 0}
+	state := NewNetworkState(3)
 	state.loads[1] = 30
 
 	state.RemoveLoad(1, 10)
 	state.RemoveLoad(1, 10)
+	gotChanges := state.Changes()
 
-	if got := state.loads[1]; got != 10 {
-		t.Errorf("Load of edge 1 should be 10, got %d", got)
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if !state.savedEdges.Contains(1) {
-		t.Errorf("Edge 1 should be saved")
-	}
-	if got := state.savedLoads[1]; got != 30 {
-		t.Errorf("Saved load of edge 1 should be 30, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestState_PersistChanges(t *testing.T) {
-	state := NewState(3)
+	wantLoads := []int64{0, 10, 20}
+	wantChanges := []LoadChange{}
+	state := NewNetworkState(3)
 
 	state.AddLoad(1, 10)
 	state.AddLoad(2, 10)
 	state.AddLoad(2, 10)
 	state.PersistChanges()
+	gotChanges := state.Changes()
 
-	if state.savedEdges.Contains(1) {
-		t.Errorf("Edge 1 should not be saved")
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if got := state.loads[1]; got != 10 {
-		t.Errorf("Load of edge 1 should be 10, got %d", got)
-	}
-	if state.savedEdges.Contains(1) {
-		t.Errorf("Edge 2 should not be saved")
-	}
-	if got := state.loads[2]; got != 20 {
-		t.Errorf("Load of edge 2 should be 20, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestState_UndoChanges(t *testing.T) {
-	state := NewState(3)
+	wantLoads := []int64{0, 10, 20}
+	wantChanges := []LoadChange{}
+	state := NewNetworkState(3)
 	state.loads[1] = 10
 	state.loads[2] = 20
 
 	state.AddLoad(1, 100)
 	state.RemoveLoad(2, 10)
 	state.UndoChanges()
+	gotChanges := state.Changes()
 
-	if state.savedEdges.Contains(1) {
-		t.Errorf("Edge 1 should not be saved")
+	for e, want := range wantLoads {
+		if got := state.Load(e); got != want {
+			t.Errorf("Load(%d): want %d, got %d", e, want, got)
+		}
 	}
-	if got := state.loads[1]; got != 10 {
-		t.Errorf("Load of edge 1 should be 10, got %d", got)
-	}
-	if state.savedEdges.Contains(1) {
-		t.Errorf("Edge 2 should not be saved")
-	}
-	if got := state.loads[2]; got != 20 {
-		t.Errorf("Load of edge 2 should be 20, got %d", got)
+	if diff := cmp.Diff(wantChanges, gotChanges); diff != "" {
+		t.Errorf("Changes(): mismatch (-want +got):\n%s", diff)
 	}
 }
 
-func TestState_ChangedEdges(t *testing.T) {
-	state := NewState(5)
-	want := []int{1, 3}
+func TestState_Changes(t *testing.T) {
+	want := []LoadChange{{1, 0}, {3, 0}}
+	state := NewNetworkState(5)
 
 	state.AddLoad(1, 50)
 	state.RemoveLoad(3, 100)
-	got := state.ChangedEdges()
+	got := state.Changes()
 
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("ChangedEdges(): mismatch (-want +got):\n%s", diff)
